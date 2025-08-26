d5c18411aa51d1f0346f8860a68a0b06bcebcc07
olsoto <thomasolson2002@gmail.com>
2025-08-26 16:37:56 -0500

    early flashcard test

---
+++ flashcard.py
@@ -0,0 +1,13 @@
+class Flashcard:
+    def __init__(self, question, answer):
+        self.answer = answer
+        self.question = question
+
+    def get_question(self):
+        return self.question
+
+    def get_answer(self):
+        return self.answer
+
+    def __str__(self):
+        return f"{self.question} - {self.answer}"
---
+++ main.py
@@ -0,0 +1,9 @@
+from flashcard import Flashcard
+def main():
+    
+    flash_card1 = Flashcard("test question", "test answer")
+    print(flash_card1)
+    return 0
+
+if __name__ == "__main__":
+    main()
